c96023519787b186cd26a067afff5eb011d6f8b9
Gustavo Luigi <=>
2023-09-04 21:00:46 -0300

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Oliveira Energia Solar</title>
+</head>
+<body>
+    <h1>Oliveira Energia Solar</h1>
+</body>
+</html>
