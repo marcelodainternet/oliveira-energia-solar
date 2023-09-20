069d16191bc41f6de06b2be46fd17886c6e533da
Gustavo Luigi <=>
2023-09-20 16:50:17 -0300

    novas rotas adicionadas

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,9 @@ Route::get('/dicas', [SiteController::class, 'dicas']);
 Route::get('/videos', [SiteController::class, 'videos']);
 Route::get('/faq', [SiteController::class, 'faq']);
 Route::post('/lead', [SiteController::class, 'leadStore']);
+Route::get('/categorias/{categoria}', [SiteController::class, 'mostrarCategoria']);
+Route::get('/privacidade', [SiteController::class, 'privacidade']);
+Route::get('/termos', [SiteController::class, 'termos']);
 
 Route::group(["prefix" => "adm"], function () {
     Route::get('/login', [AuthController::class, 'login'])->name('login');
@@ -18,15 +21,34 @@ Route::group(["prefix" => "adm"], function () {
 
     Route::group(["middleware" => "auth"], function () {
         Route::get('/', [AdminController::class, 'index']);
-        Route::get('/configuracoes', [AdminController::class, 'configuracoes']);
-        Route::post('/configuracoes', [AdminController::class, 'salvarConfiguracoes']);
-        Route::get('/secoes/{secao?}', [AdminController::class, 'secoes'])->name("secoes.editar");
-        Route::get('/secoes/{secao}', [AdminController::class, 'secoes'])->name("secoes.excluir");
-        Route::get('/secoes/{secao}/postagens/{postagem?}', [AdminController::class, 'postagens'])->name("postagens.editar");
-        Route::get('/secoes/{secao}/postagens/{postagem}', [AdminController::class, 'postagens'])->name("postagens.excluir");
-        Route::get('/projetos/{projeto?}', [AdminController::class, 'projetos'])->name("projetos.editar");
-        Route::get('/projetos/{projeto}', [AdminController::class, 'projetos'])->name("projetos.excluir");
+
+        Route::get('/configuracoes', [AdminController::class, 'editarConfiguracoes'])->name('configuracoes.editar');
+        Route::put('/configuracoes', [AdminController::class, 'atualizarConfiguracoes'])->name('configuracoes.atualizar');
+
+        Route::get('/secoes/{secao?}', [AdminController::class, 'secoes'])->name("secoes");
+        Route::put('/secoes/{secao}', [AdminController::class, 'atualizarSecao'])->name("secoes.atualizar");
+        Route::delete('/secoes/{secao}', [AdminController::class, 'excluirSecao'])->name("secoes.excluir");
+
+        Route::get('/secoes/{secao}/postagens/{postagem?}', [AdminController::class, 'postagens'])->name("postagens");
+        Route::put('/secoes/{secao}/postagens/{postagem}', [AdminController::class, 'atualizarPostagem'])->name("postagens.atualizar");
+        Route::delete('/secoes/{secao}/postagens/{postagem}', [AdminController::class, 'excluirPostagem'])->name("postagens.excluir");
+
+        Route::get('/projetos/{projeto?}', [AdminController::class, 'projetos'])->name("projetos");
+        Route::put('/projetos/{projeto}', [AdminController::class, 'atualizarProjeto'])->name("projetos.atualizar");
+        Route::delete('/projetos/{projeto}', [AdminController::class, 'excluirProjeto'])->name("projetos.excluir");
+
+        Route::get('/projetos/{projeto}/fotos/{foto?}', [AdminController::class, 'fotos'])->name("fotos");
+        Route::put('/projetos/{projeto}/fotos/{foto}', [AdminController::class, 'atualizarFoto'])->name("fotos.atualizar");
+        Route::delete('/projetos/{projeto}/fotos/{foto}', [AdminController::class, 'excluirFoto'])->name("fotos.excluir");
+
         Route::get('/leads', [AdminController::class, 'leads']);
-        Route::get('/usuarios', [AdminController::class, 'usuarios']);
+        Route::post('/leads', [AdminController::class, 'inserirLead']);
+        Route::put('/leads', [AdminController::class, 'atualizarLead']);
+        Route::delete('/leads', [AdminController::class, 'excluirLead']);
+
+        Route::get('/usuarios', [AdminController::class, 'usuarios'])->name("usuarios");
+        Route::post('/usuarios', [AdminController::class, 'inserirUsuario'])->name("usuarios.inserir");
+        Route::put('/usuarios', [AdminController::class, 'atualizarUsuario'])->name("usuarios.atualizar");
+        Route::delete('/usuarios', [AdminController::class, 'excluirUsuario'])->name("usuarios.excluir");
     });
 });
